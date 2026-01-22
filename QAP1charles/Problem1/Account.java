6e7efb292bfd5e5d14a7d257de7cc7eb6b432117
rubiacharles164-wq <rubiacharles164@gmail.com>
2026-01-22 08:50:59 -0330

    Add files via upload

---
+++ QAP1charles/Problem1/Account.java
@@ -0,0 +1,55 @@
+public class Account{
+    private String id;
+    private String name;
+    private int balance = 0;
+    //private
+
+
+
+    public Account(String id, String name){
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public Account(String id, String name, int balance){
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+    public String getID(String id){
+        return this.id;
+    }
+    public String getName(String name){
+        return this.name;
+    }
+    public int getBalance(int balance){
+        return this.balance;
+    }
+    public int credit(int amount){
+
+        return this.balance + amount;
+    }
+    public int debit(int amount){
+        if(amount <= this.balance){
+            return this.balance - amount;
+        }
+        else{
+            System.out.println("Amount exceeded balance");
+            return this.balance;
+        }
+    }
+    public int transferTo(Account another, int amount){
+        if(amount <= this.balance){
+            this.balance = this.balance - amount;
+            another.balance = another.balance + amount;
+        }
+        else{
+            System.out.println("Amount exceeded balance");
+        }
+        return this.balance;
+    }    public String toString(){
+        return "Account[id=" + this.id + ",name=" + this.name + ",balance=" + this.balance + "]";
+    }
+}
+
---
+++ QAP1charles/Problem1/TestAccount.java
@@ -0,0 +1,4 @@
+public class TestAccount {
+    public static void main(String[] args) {
+        Account acc1 = new Account("A101
+}
