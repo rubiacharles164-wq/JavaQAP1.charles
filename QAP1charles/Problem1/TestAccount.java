89e392d76b5087fe70e071c82086667f52d86cc0
rubiacharles164-wq <rubiacharles164@gmail.com>
2026-01-22 09:48:40 -0330

    Account test #1

--- QAP1charles/Problem1/TestAccount.java
+++ QAP1charles/Problem1/TestAccount.java
@@ -1,4 +1,7 @@
 public class TestAccount {
     public static void main(String[] args) {
-        Account acc1 = new Account("A101
+        Account acc1 = new Account(id: 1, name: "Alice", balance: 5000.0);
+        sytem.out.println(acc1);
+        Account acc2 = new Account(id: 2, name: "Bob", balance: 4000.0);
+        sytem.out.println(acc2);
 }
